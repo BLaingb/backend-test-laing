cd3442fee045cae349c049895b0b930075d7032a
Bernardo Laing <bernardo.laing@gmail.com>
2021-06-06 16:48:41 -0500

    Initial Menu models declaration

---
+++ cornershop-backend-test/apps/menu/models.py
@@ -0,0 +1,56 @@
+import uuid
+from django.db import models
+import datetime
+
+
+class ActiveOnlyManager(models.Manager):
+    def get_queryset(self):
+        return super(ActiveOnlyManager, self).get_queryset().filter(is_active=True)
+
+
+class MealOption(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.TextField(max_length=64, blank=False, null=False)
+    description = models.TextField(max_length=128, blank=True, default="")
+    is_active = models.BooleanField(default=True)
+
+    objects = ActiveOnlyManager()
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Menu(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    date = models.DateField(unique=True, null=False)
+    meal_options = models.ManyToManyField(MealOption, related_name="menus")
+    select_by_time = models.TimeField(default=datetime.time(15, 00))
+
+    class Meta:
+        ordering = ["-date"]
+
+    def __str__(self) -> str:
+        return f"Menu of {self.date}"
+
+
+class Meal(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, null=False)
+    selected_option = models.ForeignKey(
+        MealOption, null=False, on_delete=models.PROTECT
+    )
+    employee = models.TextField(max_length=64)
+    note = models.TextField(max_length=128, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+        constraints = [
+            models.UniqueConstraint(fields=["menu", "employee"], name="unique_meal")
+        ]
+
+    def __str__(self) -> str:
+        return f"{self.employee}"
