d846c88f4a647cecb18051a5ad550c3e65aa6879
Bernardo Laing <bernardo.laing@gmail.com>
2021-06-09 16:57:28 -0500

    Add CreateMenu tests

--- cornershop-backend-test/apps/menu/tests.py
+++ cornershop-backend-test/apps/menu/tests.py
@@ -1,7 +1,8 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, time
 from django.test import TestCase, Client
 from .models import Meal, MealOption, Menu
 from django.urls import reverse_lazy
+from django.contrib.auth.models import Permission, User
 import random
 
 # python manage.py test apps.menu.tests
@@ -23,7 +24,7 @@ class MealCreateTestCase(TestCase):
         )
         menu.meal_options.set(random.sample(meal_options, 2))
         menu.save()
-        
+
         menu = Menu.objects.create(
             date=tomorrow.date()
         )
@@ -83,3 +84,61 @@ class MealCreateTestCase(TestCase):
         self.assertEqual(len(meals), 0) # No meals for menu-employee
 
 
+class MenuCreateTestCase(TestCase):
+    def setUp(self):
+        self.meal_options = MealOption.objects.bulk_create(
+            [
+                MealOption(name="Corn pie, Salad and Dessert"),
+                MealOption(name="Chicken Nugget Rice, Salad and Dessert"),
+                MealOption(name="Rice with hamburger, Salad and Dessert"),
+                MealOption(name="Premium chicken Salad and Dessert"),
+            ]
+        )
+        permissions = Permission.objects.all()
+        user = User.objects.create_user(
+            username="testuser", email="testuser@example.com", password="testuser"
+        )
+        user.user_permissions.set(permissions)
+        user.save()
+        self.user = user
+
+    def test_can_create_menu(self):
+        c = Client()
+        options = list(map(lambda x: x.id, random.sample(self.meal_options, 2)))
+        tomorrow = (datetime.now() + timedelta(days=1)).date()
+        select_by_time = time(11, 00)
+        c.login(username='testuser', password='testuser')
+        response = c.post(
+            reverse_lazy("menu-create"),
+            {
+                "meal_options": options,
+                "date": tomorrow,
+                "select_by_time": select_by_time,
+                "notify_now": False,
+            },
+        )
+        exists = Menu.objects.all().exists()
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(exists, f'Menu with date {tomorrow} should exist.')
+
+    def test_cannot_create_duplicate_menu(self):
+        c = Client()
+        options = list(map(lambda x: x.id, random.sample(self.meal_options, 2)))
+        tomorrow = (datetime.now() + timedelta(days=1)).date()
+        select_by_time = time(11, 00)
+        c.login(username='testuser', password='testuser')
+        
+        Menu.objects.create(date=tomorrow, select_by_time=select_by_time)
+
+        response = c.post(
+            reverse_lazy("menu-create"),
+            {
+                "meal_options": options,
+                "date": tomorrow,
+                "select_by_time": select_by_time,
+                "notify_now": False,
+            },
+        )
+        menus = len(Menu.objects.filter(date=tomorrow))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(menus, 1, f'There should only be one menu for date {tomorrow}. There are {menus} menus.')
